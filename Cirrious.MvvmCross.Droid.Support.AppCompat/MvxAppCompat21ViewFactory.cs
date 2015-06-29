7849db48f9e19d2bcd8a0d885503cb36f8281581
Erik van Seters <github@dexyon.com>
2015-06-29 22:35:43 +0200

    Added a Custom AppCompact21ViewFactory and BindingBuilder. All based upon: https://gist.github.com/kjeremy/5b76cb2eb82ff6caa046

---
+++ Cirrious.MvvmCross.Droid.Support.AppCompat/MvxAppCompat21ViewFactory.cs
@@ -0,0 +1,46 @@
+﻿// MvxActivityCompat.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using Android.Content;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Cirrious.MvvmCross.Binding.Droid.Binders;
+using Cirrious.MvvmCross.Droid.Support.AppCompat.Widget;
+
+namespace Cirrious.MvvmCross.Droid.Support.AppCompat
+{
+    public class MvxAppCompat21ViewFactory : MvxAndroidViewFactory
+    {
+        private static AppCompatDelegate GetDelegate(Context context)
+        {
+            // Special case this for when you know you have an AppCompatDelegate
+            AppCompatActivity activity = context as AppCompatActivity;
+            return activity != null ? activity.Delegate : null;
+        }
+
+        public override View CreateView(View parent, string name, Context context, IAttributeSet attrs)
+        {
+            var @delegate = GetDelegate(context);
+            if (@delegate != null)
+            {
+                var view = @delegate.CreateView(parent, name, context, attrs);
+                if (view != null)
+                    return view;
+
+                if (name == "MvxSpinner")
+                {
+                    return new MvxAppCompatSpinner(context, attrs);
+                }
+            }
+
+            return base.CreateView(parent, name, context, attrs);
+        }
+    }
+}
+
